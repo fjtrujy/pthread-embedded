db5df2883d32ca649888eb0c0e8764125b1848d2
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-08-22 23:30:05 +0200

    Initial PS2 port

---
+++ platform/ps2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern void pte_test_main();
+
+int main()
+{
+  pte_test_main();
+
+  return 0;
+}
+
+
---
+++ platform/ps2/pte_osal.h
@@ -0,0 +1,13 @@
+
+#ifndef _OS_SUPPORT_H_
+#define _OS_SUPPORT_H_
+
+// Platform specific one must be included first
+typedef int pte_osThreadHandle;
+typedef int pte_osSemaphoreHandle;
+typedef int pte_osMutexHandle;
+
+#include "pte_generic_osal.h"
+
+
+#endif // _OS_SUPPORT_H
